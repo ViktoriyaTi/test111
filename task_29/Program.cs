8a3540b2450d76d04915965c3fc51dba0d9869e0
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-08-17 15:58:34 +0300

    add task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 29:
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+(на примерах демонстрация выводов 5 и 3 размерных массивов, вам же нужно сделать 8)
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] array = new int[8];
+Console.WriteLine("Введите значения массива: ");
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
