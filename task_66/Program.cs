3b78ba71a7f3071948b7fa9596e00c96698f8f51
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-09-04 10:04:54 +0300

    add_finish_task_66

--- task_66/Program.cs
+++ task_66/Program.cs
@@ -6,20 +6,19 @@ M = 1; N = 15 -> 56
 M = 4; N = 8. -> 18
 */
 
-int m = 4;
-int n = 8;
+int m = 1;
+int n = 15;
 if (m % 2 != 0)
 {
     m = m + 1;
 }
 Console.WriteLine(SumEvenNumbers(m, n));
 
-int SumEvenNumbers(int n, int m)
+int SumEvenNumbers(int m, int n)
 {
-    if (n <= m)
+    if (n >= m)
     {
-        return n + SumEvenNumbers(n + 2, m);
+        return m + SumEvenNumbers(m + 2, n);
     }
     else return 0;
 }
-
